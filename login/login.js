ef6d44b5e05b03fc2512e8dfc9c443828e599279
Rafael-Manna <rafaelmannager@gmail.com>
2025-10-27 18:18:35 -0300

    aplicalçao web :sparkles:

---
+++ login/login.js
@@ -0,0 +1,16 @@
+
+document.getElementById("botaologin").addEventListener("click", function() {
+  const email = document.getElementById("email").value;
+  const senha = document.getElementById("senha").value;
+
+  // Exemplo de validação simples, substituir depois 
+  const emailCorreto = "usuario@exemplo.com";
+  const senhaCorreta = "12345";
+
+  if(email === emailCorreto && senha === senhaCorreta){
+        window.location.href = "../home/home.html";
+    } else {
+        document.getElementById('mensagem-erro').innerHTML = ("Email ou senha incorretos");
+        
+}
+});
